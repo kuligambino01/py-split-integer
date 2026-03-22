c77cf0b9ba89f959211dfbbd8ebec70764423718
kuligambino01 <czaro923@gmail.com>
2026-03-22 12:48:35 +0100

    'fixednames'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,21 @@
 from app.split_integer import split_integer
 
 
-def test_sum_of_the_parts_should_be_equal_to_value() -> None:
+def test_should_return_value_when_split_into_single_part() -> None:
     assert split_integer(1, 1) == [1]
 
 
-def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
+def test_should_split_into_equal_parts_when_value_is_divisible_by_number_of_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
 
 
-def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
+def test_should_return_value_when_number_of_parts_is_one() -> None:
     assert split_integer(17, 1) == [17]
 
 
-def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
+def test_should_return_sorted_parts_when_value_not_divisible_equally() -> None:
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+def test_should_handle_value_less_than_number_of_parts() -> None:
     assert split_integer(2, 3) == [0, 1, 1]
