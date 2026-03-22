df8201e9c52b66dff6a638802bf16ba49168f2da
kuligambino01 <czaro923@gmail.com>
2026-03-22 12:51:42 +0100

    'solutionv5151341'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,21 +1,21 @@
 from app.split_integer import split_integer
 
 
-def test_should_return_value_when_split_into_single_part() -> None:
+def test_split_single_part() -> None:
     assert split_integer(1, 1) == [1]
 
 
-def test_should_split_into_equal_parts_when_value_is_divisible_by_number_of_parts() -> None:
+def test_split_equal_parts() -> None:
     assert split_integer(6, 2) == [3, 3]
 
 
-def test_should_return_value_when_number_of_parts_is_one() -> None:
+def test_return_value_when_one_part() -> None:
     assert split_integer(17, 1) == [17]
 
 
-def test_should_return_sorted_parts_when_value_not_divisible_equally() -> None:
+def test_split_sorted_when_not_divisible() -> None:
     assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
-def test_should_handle_value_less_than_number_of_parts() -> None:
+def test_split_when_value_less_than_parts() -> None:
     assert split_integer(2, 3) == [0, 1, 1]
